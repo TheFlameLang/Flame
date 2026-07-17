53f5a5c4ab066214d8fe97ca1e1a86d2f9b43fd0
Naharashu <130159705+Naharashu@users.noreply.github.com>
2026-07-17 23:05:24 +0300

    dont use this ver

---
+++ shiver/testdir/a_flame.cpp
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <oxygen_runtime.h>
+namespace stdlib {
+#include <iostream>
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <memory>
+int64_t abs(int64_t a)  {
+    if((a > 0)) {
+        return a;
+    };
+    return -a;
+}
+
+const double PI=3.140000;
+double sqrt(double a)  {
+    double x=1.000000;
+     for(int32_t i=0;(i <= 6);i++) {
+        x=(0.500000 * (x + (a / x)));
+    };
+    return x;
+}
+
+double floor_helper(double a)  {
+    double res=a;
+    if(((a < 0) && (a != res))) {
+        return (res - 1);
+    };
+    return res;
+}
+
+double floor(double a)  {
+    if(((a - stdlib.flame_._floor_helper(a)) < 0.500000)) {
+        return stdlib.flame_._floor_helper(a);
+    };
+    return stdlib.flame_._floor_helper((a + 1));
+}
+
+double fmod(double a, double b)  {
+    double res=(a - (b * stdlib.flame_._floor((a / b))));
+    return res;
+}
+
+double pow(double a, int8_t b)  {
+    double res=a;
+     for(int32_t i=1;(i < b);i++) {
+        res *= a;
+    };
+    return res;
+}
+
+double fshiftr(double a, int8_t n)  {
+    return (a / stdlib.flame_._pow(2, n));
+}
+
+double fshiftl(double a, int8_t n)  {
+    return (a * stdlib.flame_._pow(2, n));
+}
+
+void println(auto s)  {
+    std::cout << s;
+    std::cout << "\n";
+    return;
+}
+
+}
+namespace strings {
+#include <iostream>
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <memory>
+uint32_t len(std::string s)  {
+    uint32_t len=0;
+    while((s[len] != 0))  {
+        len++;
+    };
+    return len;
+}
+
+std::string concat(std::string s1, std::string s2)  {
+    std::string s3=s1;
+    s3 += s2;
+    return s3;
+}
+
+std::string reversed(std::string s)  {
+    uint32_t size=strings.flame_._len(s);
+    std::string rev="";
+     for(int32_t i=(size - 1);(i >= 0);i--) {
+        rev += s[i];
+    };
+    return rev;
+}
+
+std::string substr(std::string s, int64_t start, int64_t length)  {
+    uint32_t size=strings.flame_._len(s);
+    if((((start < 0) || (length < 0)) || ((start + length) > size))) {
+        return "";
+    };
+    if(((start == 0) && (length == size))) {
+        return s;
+    };
+    std::string sub="";
+     for(uint64_t i=start;(i < (start + length));i++) {
+        sub += s[i];
+    };
+    return sub;
+}
+
+int64_t indexOf(std::string s, std::string sub)  {
+    uint32_t size=strings.flame_._len(s);
+    uint32_t ssize=strings.flame_._len(sub);
+    if(((size == 0) || (size < ssize))) {
+        return -1;
+    };
+     for(uint64_t i=0;(i < size);i++) {
+        bool match=1;
+         for(uint64_t j=0;(j < ssize);j++) {
+            if((s[i] != sub[j])) {
+                match=0;
+                break;
+            };
+        };
+        if(match) {
+            return i;
+        };
+    };
+    return -1;
+}
+
+bool contains(std::string s, std::string sub)  {
+    bool cont=(strings.flame_._indexOf(s, sub) != -1);
+    return cont;
+}
+
+}
+struct point {
+    int32_t x=1;
+    int32_t y=1;
+};
+float test(const float  *const a)  {
+    return a;
+}
+
+int32_t main()  {
+    int32_t a=10;
+    a ^= 20;
+    return 0;
+}
+
